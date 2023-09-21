28ff2c71342914d35b5a5dbf307c1fb80bb57988
CeciCanedo <ceciliacanedolozada@gmail.com>
2023-09-21 12:19:03 -0600

    Bucle for, while y do-while

--- arreglos-bucles/js/arreglos.js
+++ arreglos-bucles/js/arreglos.js
@@ -126,7 +126,10 @@ function agregarArticulo(nuevoArticulo){
  * 
  */
 
-//FOR
+
+// PARTE 2 BUCLES
+
+// BUCLE FOR
 
 /** Bucle for 
  * 
@@ -209,4 +212,150 @@ for (let animal in animales){
 for (let listaNumeros=1; listaNumeros <=20; listaNumeros++){
     if (listaNumeros % 2 ===0)
     console.log(`los numeros pares de la lista son ${listaNumeros}`);
+}
+
+
+// Imprimir la tabla de multiplicar del 8 con output:
+// 8 x 1 = 8...
+const multiplicando=12;
+for (i=1; i<=10; i++){
+    let resultado=multiplicando*i;
+    console.log(`${multiplicando} x ${i} =${resultado}`)
+}
+
+function tablaMultiplicar(numero){
+    for (i=1; i<=10; i++){
+        let resultado= numero*i;
+        console.log(`${numero}x${i}=${resultado}`)
+    }
+}
+tablaMultiplicar(12);
+
+const tablaFlecha=(numero)=> {
+    for (i=1; i<=10; i++){
+        let resultado= numero*i;
+        console.log(`${numero}x${i}=${resultado}`)
+}
+}
+tablaFlecha(10);
+
+// BUCLE WHILE
+
+/** Bucle while recorre un bucle de codigo siempre que una condicion sea 
+ * verdadera. No conocemos la cantidad de veces que la estructura repetira
+ * las instrucciones, aunque si se puede controlar.
+ * 
+ * Sintaxis
+ * while(condition){
+ *      code block
+ * }
+ */
+
+let productos=10;
+while(productos>0){
+    console.log("Productos vendidos" + productos);
+    productos--;
+}
+
+//Imprimir los numero del 1 al 6
+let numero= 0;
+while(numero<6){
+    numero++;
+    console.log(numero); //inicia en 0 porque si no ignora el primer indice (0=1)
+}
+
+let cuenta= 1;
+while(cuenta<=6){
+    console.log(cuenta);
+    cuenta++;
+}
+
+/** La diferencia entre los ejemplos anteriores es el simbolo < y <=. 
+ * En el primer ejemplo al tener la iteracion antes que la impresion si se
+ * usa el simbolo <= se realiza una ultima iteracion con el 6, llegando 
+ * hasta el 7 y se detiene , mientras que en el segundo ejemplo al tener
+ * la iteracion despues de la impresion llega hasta 6 solamente.
+*/
+
+let ropero=["chamarra", "camisa", "playera", "bermuda", "guayabera", "pantalon", "falda",
+"vestido", "jogger", "pants", "sueter", "saco", "jumper"]
+let ropa=0
+while(ropa<ropero.length){
+    console.log(ropero[ropa]);//(array[elementos])
+    ropa++;
+}
+
+//Crear una secuencia creciente de 5 asteriscos
+/**
+ * 
+ * *
+ * * *
+ * * * *
+ * * * * *
+ */
+
+let limite= 10;
+let index= 0;
+let texto= "";
+
+while(index<limite){
+    texto= texto + "*";
+    console.log(texto);
+    index++;
+}
+
+// BUCLE DO-WHILE
+
+/** Bucle do-while
+ * 
+ * En este bucle la condicion se evalua despues de ejecutar la sentencia,
+ * es decir, la sentencia especifica se ejecuta al menos una vez. Primero 
+ * se ejecutan las acciones y despues pregunta si se puede continuar 
+ * ejecutando dichas acciones.
+ * 
+ * Sintaxis
+ * do {
+ *      codeblock
+ * } while(conditio);
+ * 
+ */
+
+//Cuenta del 0 al 6 utilizando un ciclo Do-While
+
+let numeroInicial= 0;
+do{
+    console.log(`Utilizando Do-while ${numeroInicial}`);
+    numeroInicial++;
+} while(numeroInicial<= 6);
+
+//CONTROL DE CICLOS
+
+/** Control de ciclos
+ * 
+ * break - Es una sentencia que permite al programa salir. Debe encontrarse 
+ *         anidado dentro del bucle
+ * continue - No termina el bucle, sino que salta a la siguiente iteracion
+ * 
+ */
+
+/** Super Oxxito. Si ere el cliente numero 5, te ganas una coca-cola en
+ * bolsita.
+ * 
+ */
+
+//Con break
+for(let cliente =1; cliente <=50; cliente++){
+    if (cliente===5){
+        console.log("Eres el cliente numero 5, te ganaste una coca-cola en bolsita")
+        break;
+    }
+    console.log(`Cliente numero ${cliente}`)
+}
+//Con continue
+for(let cliente =1; cliente <=50; cliente++){
+    if (cliente===5){
+        console.log("Eres el cliente numero 5, te ganaste una coca-cola en bolsita")
+        continue;
+    }
+    console.log(`Cliente numero ${cliente}`)
 }
